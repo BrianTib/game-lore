2e07a141a5543f221343bbb22dcddc8004ccda7f
Julia Eason <jmeason00@gmail.com>
2024-06-12 14:33:23 -0400

    commiting to create a new issue

--- assets/js/fortnite.js
+++ assets/js/fortnite.js
@@ -1,49 +1,23 @@
 
 
-
-const APIKey=''
-
-// Include the axios library
-const axios = require('axios');
-
-// Set the API endpoint and parameters
-// const API_ENDPOINT = '';
-// const headers = {
-// 'TRN-Api-Key': 'code',
-// };
-// const username = 'Ninja';
-// const platform = 'pc';
-
-// // Make the request
-// axios.get(${API_ENDPOINT}/${platform}/${username}, { headers })
-// .then((response) => {
-// const stats = response.data.lifeTimeStats;
-// console.log(${username} has ${stats[7].value} total kills);
-// })
-// .catch((error) => {
-// console.error(“errorrrrrr”, error);
-// });
+const apiKey =''
 
 
-$('#search-btn').on('click', function (e) {
-  e.preventDefault();
-  const character = $('#character-input').val().trim();
-  if (character) {
-      saveToSearchHistory(character);
-      fetchGeo(character);
-      $('#character-input').val('');
+$(document).ready(function() {
+  const apiKey = '';  //  get API key
+
+  // Function to fetch character facts
+  function fetchCharacterFacts(character) {
+      $.ajax({
+          url: `https://api.fortnitetracker.com/v1/profile/account/${character}`,
+          method: 'GET',
+          headers: {
+              'TRN-Api-Key': apiKey
+          },
+          success: function(data) {
+              console.log(data);
+              displayCharacterFacts(data);
+          }
+        })
   }
-});
-
-
-fetch('https://fortnite-api.com/v1/map', {
-    // The browser fetches the resource from the remote server without first looking in the cache.
-    // The browser will then update the cache with the downloaded resource.
-    cache: 'reload',
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
+}      
