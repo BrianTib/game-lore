9afadee2518011479ad25dda5ba405b0b15c3c0f
Julia Eason <jmeason00@gmail.com>
2024-06-13 11:54:09 -0400

    hookup endpoint to api for the news section

--- assets/js/fortnite.js
+++ assets/js/fortnite.js
@@ -1,16 +1,19 @@
-
-const characterButton= document.getElementById('character-search-btn')
+//dependencies
+const newsButton= document.getElementById('news-search-btn')
 const mapButton=document.getElementById('map-search-btn')
 const statButton=document.getElementById('stat-search-btn')
+const apiKey ='cf9a481a-5823-4216-ba41-5deccd3e6556'
 
-characterButton.addEventListener('click', handleCharacterButtonClick)
 
-function handleCharacterButtonClick(event){
-  console.log('character button')
+newsButton.addEventListener('click', handleNewsButtonClick)
+
+function handleNewsButtonClick(event){
+  getNewsDataFromAPI()
+  console.log('News button')
 }
 
 
-mapButton.addEventListener('click', handleMapButtonClick)
+mapButton.addEventListener('click', handleMapButtonClick) 
 
 function handleMapButtonClick(event){
   console.log('map button')
@@ -21,24 +24,58 @@ statButton.addEventListener('click', handleStatClick)
 function handleStatClick (event){
   console.log('stat click')
 }
-// const apiKey =''
+
+function getNewsDataFromAPI(){
+   fetch('https://fortnite-api.com/v2/news', {
+    headers: {
+      Authorization: apiKey
+    }
+   }) //replace with new api url
+      .then((response) => {
+          // Check if the request was successful
+          if (response.ok) {
+              // Retrieve the JSON information
+              // we got from the request
+              return response.json()
+          }
+      })
+      // If this next .then() executes, that means we got data from the API
+      .then((data) => {
+        console.log(data)
+          // Do something with the data
+      })
+}
+
+function getMapDataFromAPI(){
+  fetch('') //replace with map api url
+     .then((response) => {
+         // Check if the request was successful
+         if (response.ok) {
+             // Retrieve the JSON information
+             // we got from the request
+             return response.json()
+         }
+     })
+     // If this next .then() executes, that means we got data from the API
+     .then((data) => {
+         // Do something with the data
+     })
+}
+
+function getStatsDataFromAPI(){
+  fetch('') //replace with own stat api url
+     .then((response) => {
+         // Check if the request was successful
+         if (response.ok) {
+             // Retrieve the JSON information
+             // we got from the request
+             return response.json()
+         }
+     })
+     // If this next .then() executes, that means we got data from the API
+     .then((data) => {
+         // Do something with the data
+     })
+}
 
 
-// $(document).ready(function() {
-//   const apiKey = '';  //  get API key
-
-//   // Function to fetch character facts
-//   function fetchCharacterFacts(character) {
-//       $.ajax({
-//           url: `https://api.fortnitetracker.com/v1/profile/account/${character}`,
-//           method: 'GET',
-//           headers: {
-//               'TRN-Api-Key': apiKey
-//           },
-//           success: function(data) {
-//               console.log(data);
-//               displayCharacterFacts(data);
-//           }
-//         })
-//   }
-//   })  
