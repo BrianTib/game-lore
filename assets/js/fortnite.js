2cc6a3fb9b48e23d49b83b9bfeab148e3cf7323a
Julia Eason <jmeason00@gmail.com>
2024-06-13 09:48:19 -0400

    attaching buttons to event listener with get element by id

--- assets/js/fortnite.js
+++ assets/js/fortnite.js
@@ -1,23 +1,44 @@
 
+const characterButton= document.getElementById('character-search-btn')
+const mapButton=document.getElementById('map-search-btn')
+const statButton=document.getElementById('stat-search-btn')
 
-const apiKey =''
+characterButton.addEventListener('click', handleCharacterButtonClick)
+
+function handleCharacterButtonClick(event){
+  console.log('character button')
+}
 
 
-$(document).ready(function() {
-  const apiKey = '';  //  get API key
+mapButton.addEventListener('click', handleMapButtonClick)
 
-  // Function to fetch character facts
-  function fetchCharacterFacts(character) {
-      $.ajax({
-          url: `https://api.fortnitetracker.com/v1/profile/account/${character}`,
-          method: 'GET',
-          headers: {
-              'TRN-Api-Key': apiKey
-          },
-          success: function(data) {
-              console.log(data);
-              displayCharacterFacts(data);
-          }
-        })
-  }
-  })  
+function handleMapButtonClick(event){
+  console.log('map button')
+}
+
+statButton.addEventListener('click', handleStatClick)
+
+function handleStatClick (event){
+  console.log('stat click')
+}
+// const apiKey =''
+
+
+// $(document).ready(function() {
+//   const apiKey = '';  //  get API key
+
+//   // Function to fetch character facts
+//   function fetchCharacterFacts(character) {
+//       $.ajax({
+//           url: `https://api.fortnitetracker.com/v1/profile/account/${character}`,
+//           method: 'GET',
+//           headers: {
+//               'TRN-Api-Key': apiKey
+//           },
+//           success: function(data) {
+//               console.log(data);
+//               displayCharacterFacts(data);
+//           }
+//         })
+//   }
+//   })  
